636ce075ae9e8eddd83eac70416c5119ece388ed
Janderson P. Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2021-08-20 14:25:15 -0300

    feat: Creating Application Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import { Routes } from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { Dashboard } from '../pages/Dashboard';
 import { Repository } from '../pages/Repository';
 
 export function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/repository" component={Repository} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repository" component={Repository} />
+    </Switch>
   );
 }
